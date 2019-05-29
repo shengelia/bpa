6be02eaf1ca33b6ed4ec115fa99fb8a7fc801c71
samira <s.shengelia@bpa-solutions.org>
2019-05-29 16:07:12 +0400

    changes

--- 00 - structure/index.php
+++ 00 - structure/index.php
@@ -95,42 +95,42 @@
 		</div>
 	</div>
 
-	<div class="cards-header">
+	<div class="table-header">
 		<div class="container ">
-			<div class="cards-header">
-				<div class="table-name-1-number"><span>#</span></div>
-				<div class="table-name-2-casino-logo"><span>Casino</span></div>
-				<div class="table-name-3-address"><span>Address</span></div>
-				<div class="table-name-4-ratings"><span>Ratings</span></div>
-				<div class="table-name-5-our-score"><span>Our Score</span></div>
-				<div class="table-name-6-visit-site"><span>Visit Site</span></div>
+			<div class="table-header-items">
+				<div class="table-header-item-1-number"><span>#</span></div>
+				<div class="table-header-item-2-casino"><span>Casino</span></div>
+				<div class="table-header-item-3-address"><span>Address</span></div>
+				<div class="table-header-item-4-rating"><span>Rating</span></div>
+				<div class="table-header-item-5-score"><span>Our Score</span></div>
+				<div class="table-header-item-6-visit"><span></span></div>
 			</div>
 		</div>
 	</div>
 
 	<div class="hotels" id="hotels">
-		<div class="container ">
+		<div class="container">
 			<div class="filter-body">
 				<div filter="bl, s" class="cards card-item cards-mob" id="card-1">
-					<div class="bc-card-rating-position">
-						<div class="editors-choise">
+					<div class="card-item-1-number">
+						<div class="editors-choice">
 							<img src="img/badge-with-a-star.png" alt="Editors choise" width="100%">
 						</div>
 						<span>1</span>
 					</div>
-					<div class="bc-card-casino-logo">
+					<div class="card-item-2-casino">
 						<a href="#">
 							<img src="img/casino-img.png" alt="Hotel Logo" width="100%" class="hotel-logo-desktop">
 							<img src="img/bad_bentheim_casino.png" alt="Hotel Logo" width="100%"
 							class="hotel-logo-mobile">
 						</a>
 					</div>
-					<div class="bc-card-address">
+					<div class="card-item-3-address">
 						<span>
 							<p>6380 Fallsview BLVD. PO Box 300 Niagara Falls, ON L2G 7X5</p>
 						</span>
 					</div>
-					<div class="bc-card-ratings">
+					<div class="card-item-4-rating">
 						<div class="bc-star-rating">
 							<div class="ratings">
 								<div class="rate_message"></div>
@@ -142,271 +142,13 @@
 							</div>
 						</div>
 					</div>
-					<div class="bc-card-our-score">
+					<div class="card-item-5-score">
 						<div class="our-score-total">
 							<span>9.8</span>
 							<p>Our Score</p>
 						</div>
 					</div>
-					<div class="bc-card-visit-site">
-						<button>
-						<a href="#">
-							Visit
-							<span>
-								<img src="img/arrow.png" alt="Visit Website" width="100%">
-							</span>
-						</a>
-						</button>
-					</div>
-				</div>
-				<div filter="r" class="cards card-item cards-mob" id="card-2">
-					<div class="bc-card-rating-position">
-						<span>2</span>
-					</div>
-					<div class="bc-card-casino-logo">
-						<a href="">
-							<img src="img/casino-img.png" alt="Hotel Logo" width="100%" class="hotel-logo-desktop">
-							<img src="img/bad_bentheim_casino.png" alt="Hotel Logo" width="100%"
-							class="hotel-logo-mobile">
-						</a>
-					</div>
-					<div class="bc-card-address">
-						<span>6380 Fallsview BLVD. PO Box 300 Niagara Falls, ON L2G 7X5</span>
-					</div>
-					<div class="bc-card-ratings">
-						<div class="bc-star-rating">
-							<div class="ratings">
-								<div class="rate_message"></div>
-								<div class="rating not-rated" data-rate-value="1"></div>
-								<div class="rated-info">
-									<p>Rated: <strong class="rated">604</strong></p>
-									<a href="review-all.html">Review</a>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="bc-card-our-score">
-						<div class="our-score-total">
-							<span>9.8</span>
-							<p>Our Score</p>
-						</div>
-					</div>
-					<div class="bc-card-visit-site">
-						<button>
-						<a href="#">
-							Visit
-							<span>
-								<img src="img/arrow.png" alt="Visit Website" width="100%">
-							</span>
-						</a>
-						</button>
-					</div>
-				</div>
-				<div filter="p" class="cards card-item cards-mob" id="card-3">
-					<div class="bc-card-rating-position">
-						<span>3</span>
-					</div>
-					<div class="bc-card-casino-logo">
-						<a href="">
-							<img src="img/casino-img.png" alt="Hotel Logo" width="100%" class="hotel-logo-desktop">
-							<img src="img/bad_bentheim_casino.png" alt="Hotel Logo" width="100%"
-							class="hotel-logo-mobile">
-						</a>
-					</div>
-					<div class="bc-card-address">
-						<span>6380 Fallsview BLVD. PO Box 300 Niagara Falls, ON L2G 7X5</span>
-					</div>
-					<div class="bc-card-ratings">
-						<div class="bc-star-rating">
-							<div class="ratings">
-								<div class="rate_message"></div>
-								<div class="rating not-rated" data-rate-value="5"></div>
-								<div class="rated-info">
-									<p>Rated: <strong class="rated">604</strong></p>
-									<a href="review-all.html">Review</a>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="bc-card-our-score">
-						<div class="our-score-total">
-							<span>9.8</span>
-							<p>Our Score</p>
-						</div>
-					</div>
-					<div class="bc-card-visit-site">
-						<button>
-						<a href="#">
-							Visit
-							<span>
-								<img src="img/arrow.png" alt="Visit Website" width="100%">
-							</span>
-						</a>
-						</button>
-					</div>
-				</div>
-				<div filter="bl, ba" class="cards card-item cards-mob" id="card-4">
-					<div class="bc-card-rating-position">
-						<span>4</span>
-					</div>
-					<div class="bc-card-casino-logo">
-						<a href="">
-							<img src="img/casino-img.png" alt="Hotel Logo" width="100%" class="hotel-logo-desktop">
-							<img src="img/bad_bentheim_casino.png" alt="Hotel Logo" width="100%"
-							class="hotel-logo-mobile">
-						</a>
-					</div>
-					<div class="bc-card-address">
-						<span>6380 Fallsview BLVD. PO Box 300 Niagara Falls, ON L2G 7X5</span>
-					</div>
-					<div class="bc-card-ratings">
-						<div class="bc-star-rating">
-							<div class="ratings">
-								<div class="rate_message"></div>
-								<div class="rating not-rated" data-rate-value="5"></div>
-								<div class="rated-info">
-									<p>Rated: <strong class="rated">604</strong></p>
-									<a href="review-all.html">Review</a>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="bc-card-our-score">
-						<div class="our-score-total">
-							<span>9.8</span>
-							<p>Our Score</p>
-						</div>
-					</div>
-					<div class="bc-card-visit-site">
-						<button>
-						<a href="#">
-							Visit
-							<span>
-								<img src="img/arrow.png" alt="Visit Website" width="100%">
-							</span>
-						</a>
-						</button>
-					</div>
-				</div>
-				<div filter="ba" class="cards card-item cards-mob" id="card-5">
-					<div class="bc-card-rating-position">
-						<span>5</span>
-					</div>
-					<div class="bc-card-casino-logo">
-						<a href="">
-							<img src="img/casino-img.png" alt="Hotel Logo" width="100%" class="hotel-logo-desktop">
-							<img src="img/bad_bentheim_casino.png" alt="Hotel Logo" width="100%"
-							class="hotel-logo-mobile">
-						</a>
-					</div>
-					<div class="bc-card-address">
-						<span>6380 Fallsview BLVD. PO Box 300 Niagara Falls, ON L2G 7X5</span>
-					</div>
-					<div class="bc-card-ratings">
-						<div class="bc-star-rating">
-							<div class="ratings">
-								<div class="rate_message"></div>
-								<div class="rating not-rated" data-rate-value="5"></div>
-								<div class="rated-info">
-									<p>Rated: <strong class="rated">604</strong></p>
-									<a href="review-all.html">Review</a>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="bc-card-our-score">
-						<div class="our-score-total">
-							<span>9.8</span>
-							<p>Our Score</p>
-						</div>
-					</div>
-					<div class="bc-card-visit-site">
-						<button>
-						<a href="#">
-							Visit
-							<span>
-								<img src="img/arrow.png" alt="Visit Website" width="100%">
-							</span>
-						</a>
-						</button>
-					</div>
-				</div>
-				<div filter="bl" class="cards card-item cards-mob" id="card-6">
-					<div class="bc-card-rating-position">
-						<span>6</span>
-					</div>
-					<div class="bc-card-casino-logo">
-						<a href="">
-							<img src="img/casino-img.png" alt="Hotel Logo" width="100%" class="hotel-logo-desktop">
-							<img src="img/bad_bentheim_casino.png" alt="Hotel Logo" width="100%"
-							class="hotel-logo-mobile">
-						</a>
-					</div>
-					<div class="bc-card-address">
-						<span>6380 Fallsview BLVD. PO Box 300 Niagara Falls, ON L2G 7X5</span>
-					</div>
-					<div class="bc-card-ratings">
-						<div class="bc-star-rating">
-							<div class="ratings">
-								<div class="rate_message"></div>
-								<div class="rating not-rated" data-rate-value="5"></div>
-								<div class="rated-info">
-									<p>Rated: <strong class="rated">604</strong></p>
-									<a href="review-all.html">Review</a>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="bc-card-our-score">
-						<div class="our-score-total">
-							<span>9.8</span>
-							<p>Our Score</p>
-						</div>
-					</div>
-					<div class="bc-card-visit-site">
-						<button>
-						<a href="#">
-							Visit
-							<span>
-								<img src="img/arrow.png" alt="Visit Website" width="100%">
-							</span>
-						</a>
-						</button>
-					</div>
-				</div>
-				<div filter="bl" class="cards card-item cards-mob" id="card-7">
-					<div class="bc-card-rating-position">
-						<span>7</span>
-					</div>
-					<div class="bc-card-casino-logo">
-						<a href="">
-							<img src="img/casino-img.png" alt="Hotel Logo" width="100%" class="hotel-logo-desktop">
-							<img src="img/bad_bentheim_casino.png" alt="Hotel Logo" width="100%"
-							class="hotel-logo-mobile">
-						</a>
-					</div>
-					<div class="bc-card-address">
-						<span>6380 Fallsview BLVD. PO Box 300 Niagara Falls, ON L2G 7X5</span>
-					</div>
-					<div class="bc-card-ratings">
-						<div class="bc-star-rating">
-							<div class="ratings">
-								<div class="rate_message"></div>
-								<div class="rating not-rated" data-rate-value="5"></div>
-								<div class="rated-info">
-									<p>Rated: <strong class="rated">604</strong></p>
-									<a href="review-all.html">Review</a>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="bc-card-our-score">
-						<div class="our-score-total">
-							<span>9.8</span>
-							<p>Our Score</p>
-						</div>
-					</div>
-					<div class="bc-card-visit-site">
+					<div class="card-item-6-visit">
 						<button>
 						<a href="#">
 							Visit
