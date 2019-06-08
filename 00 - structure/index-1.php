00653d9e541aab491204b8165f7139dad448f63b
samira <s.shengelia@bpa-solutions.org>
2019-06-08 19:52:46 +0400

    changes

--- 00 - structure/index-1.php
+++ 00 - structure/index-1.php
@@ -51,8 +51,8 @@
 				<div class="interior-table-header-item-1-number"><span>#</span></div>
 				<div class="interior-table-header-item-2-casino-logo"><span>Casino</span></div>
 				<div class="interior-table-header-item-3-address"><span>Adress</span></div>
-				<div class="interior-table-header-item-4-score"><span>Our Score</span></div>
-				<div class="interior-table-header-item-5-rating"><span>Rating</span></div>
+				<div class="interior-table-header-item-4-rating"><span>Rating</span></div>
+				<div class="interior-table-header-item-5-score"><span>Our Score</span></div>
 				<div class="interior-table-header-item-6-visit"><span>Visit Our Website</span></div>
 			</div>
 		</div>
@@ -78,13 +78,7 @@
 							<h1>Delectus ea quibusdam incidunt, debitis!</h1>
 						</span>
 					</div>
-					<div class="interior-card-item-4-score">
-						<div class="our-score">
-							<span>9.8</span>
-							<p>Our score</p>
-						</div>
-					</div>					
-					<div class="interior-card-item-5-rating">
+					<div class="interior-card-item-4-rating">
 						<div class="ratings">
 							<div class="rate_message"></div>
 							<div class="rating not-rated" data-rate-value="3"></div>
@@ -92,17 +86,23 @@
 								<a href="review-single.php">Review</a>
 							</div>
 						</div>
-					</div>					
+					</div>
+					<div class="interior-card-item-5-score">
+						<div class="our-score">
+							<span>9.8</span>
+							<p>Our score</p>
+						</div>
+					</div>										
 					<div class="interior-card-item-6-visit">
 						<span>
 							<form action="http://www.mail.ru" target="_blank">
-							    <button type="submit"><img src="img/button_visit_arrow.png" alt="Visit"><p>Visit Our Site</p></button>
+							    <button type="submit"><p>Visit Our Site</p></button>
 							</form>
 						</span>						
 					</div>
 				</div>
 
-				<div filter="bl" class="cards card-item cards-mob" id="card-2">
+				<div filter="bl, s" class="cards card-item cards-mob" id="card-2">
 					<div class="interior-card-item-1-number">
 						<span>2</span>
 					</div>
@@ -116,13 +116,7 @@
 							<h1>Delectus ea quibusdam incidunt, debitis!</h1>
 						</span>
 					</div>
-					<div class="interior-card-item-4-score">
-						<div class="our-score">
-							<span>9.8</span>
-							<p>Our score</p>
-						</div>
-					</div>					
-					<div class="interior-card-item-5-rating">
+					<div class="interior-card-item-4-rating">
 						<div class="ratings">
 							<div class="rate_message"></div>
 							<div class="rating not-rated" data-rate-value="3"></div>
@@ -130,17 +124,23 @@
 								<a href="review-single.php">Review</a>
 							</div>
 						</div>
-					</div>					
+					</div>
+					<div class="interior-card-item-5-score">
+						<div class="our-score">
+							<span>9.8</span>
+							<p>Our score</p>
+						</div>
+					</div>										
 					<div class="interior-card-item-6-visit">
 						<span>
 							<form action="http://www.mail.ru" target="_blank">
-							    <button type="submit"><img src="img/button_visit_arrow.png" alt="Visit"><p>Visit Our Site</p></button>
+							    <button type="submit"><p>Visit Our Site</p></button>
 							</form>
 						</span>						
 					</div>
 				</div>
 
-				<div filter="bl" class="cards card-item cards-mob" id="card-3">
+				<div filter="p" class="cards card-item cards-mob" id="card-3">
 					<div class="interior-card-item-1-number">
 						<span>3</span>
 					</div>
@@ -154,13 +154,7 @@
 							<h1>Delectus ea quibusdam incidunt, debitis!</h1>
 						</span>
 					</div>
-					<div class="interior-card-item-4-score">
-						<div class="our-score">
-							<span>9.8</span>
-							<p>Our score</p>
-						</div>
-					</div>					
-					<div class="interior-card-item-5-rating">
+					<div class="interior-card-item-4-rating">
 						<div class="ratings">
 							<div class="rate_message"></div>
 							<div class="rating not-rated" data-rate-value="3"></div>
@@ -168,17 +162,23 @@
 								<a href="review-single.php">Review</a>
 							</div>
 						</div>
-					</div>					
+					</div>
+					<div class="interior-card-item-5-score">
+						<div class="our-score">
+							<span>9.8</span>
+							<p>Our score</p>
+						</div>
+					</div>										
 					<div class="interior-card-item-6-visit">
 						<span>
 							<form action="http://www.mail.ru" target="_blank">
-							    <button type="submit"><img src="img/button_visit_arrow.png" alt="Visit"><p>Visit Our Site</p></button>
+							    <button type="submit"><p>Visit Our Site</p></button>
 							</form>
 						</span>						
 					</div>
 				</div>
 
-				<div filter="p" class="cards card-item cards-mob" id="card-4">
+				<div filter="ba" class="cards card-item cards-mob" id="card-4">
 					<div class="interior-card-item-1-number">
 						<span>4</span>
 					</div>
@@ -192,13 +192,7 @@
 							<h1>Delectus ea quibusdam incidunt, debitis!</h1>
 						</span>
 					</div>
-					<div class="interior-card-item-4-score">
-						<div class="our-score">
-							<span>9.8</span>
-							<p>Our score</p>
-						</div>
-					</div>					
-					<div class="interior-card-item-5-rating">
+					<div class="interior-card-item-4-rating">
 						<div class="ratings">
 							<div class="rate_message"></div>
 							<div class="rating not-rated" data-rate-value="3"></div>
@@ -206,17 +200,23 @@
 								<a href="review-single.php">Review</a>
 							</div>
 						</div>
-					</div>					
+					</div>
+					<div class="interior-card-item-5-score">
+						<div class="our-score">
+							<span>9.8</span>
+							<p>Our score</p>
+						</div>
+					</div>										
 					<div class="interior-card-item-6-visit">
 						<span>
 							<form action="http://www.mail.ru" target="_blank">
-							    <button type="submit"><img src="img/button_visit_arrow.png" alt="Visit"><p>Visit Our Site</p></button>
+							    <button type="submit"><p>Visit Our Site</p></button>
 							</form>
 						</span>						
 					</div>
 				</div>
 
-				<div filter="p" class="cards card-item cards-mob" id="card-5">
+				<div filter="ba" class="cards card-item cards-mob" id="card-5">
 					<div class="interior-card-item-1-number">
 						<span>5</span>
 					</div>
@@ -230,13 +230,7 @@
 							<h1>Delectus ea quibusdam incidunt, debitis!</h1>
 						</span>
 					</div>
-					<div class="interior-card-item-4-score">
-						<div class="our-score">
-							<span>9.8</span>
-							<p>Our score</p>
-						</div>
-					</div>					
-					<div class="interior-card-item-5-rating">
+					<div class="interior-card-item-4-rating">
 						<div class="ratings">
 							<div class="rate_message"></div>
 							<div class="rating not-rated" data-rate-value="3"></div>
@@ -244,17 +238,23 @@
 								<a href="review-single.php">Review</a>
 							</div>
 						</div>
-					</div>					
+					</div>
+					<div class="interior-card-item-5-score">
+						<div class="our-score">
+							<span>9.8</span>
+							<p>Our score</p>
+						</div>
+					</div>										
 					<div class="interior-card-item-6-visit">
 						<span>
 							<form action="http://www.mail.ru" target="_blank">
-							    <button type="submit"><img src="img/button_visit_arrow.png" alt="Visit"><p>Visit Our Site</p></button>
+							    <button type="submit"><p>Visit Our Site</p></button>
 							</form>
 						</span>						
 					</div>
 				</div>
 
-				<div filter="p" class="cards card-item cards-mob" id="card-6">
+				<div filter="s" class="cards card-item cards-mob" id="card-6">
 					<div class="interior-card-item-1-number">
 						<span>6</span>
 					</div>
@@ -268,13 +268,7 @@
 							<h1>Delectus ea quibusdam incidunt, debitis!</h1>
 						</span>
 					</div>
-					<div class="interior-card-item-4-score">
-						<div class="our-score">
-							<span>9.8</span>
-							<p>Our score</p>
-						</div>
-					</div>					
-					<div class="interior-card-item-5-rating">
+					<div class="interior-card-item-4-rating">
 						<div class="ratings">
 							<div class="rate_message"></div>
 							<div class="rating not-rated" data-rate-value="3"></div>
@@ -282,17 +276,23 @@
 								<a href="review-single.php">Review</a>
 							</div>
 						</div>
-					</div>					
+					</div>
+					<div class="interior-card-item-5-score">
+						<div class="our-score">
+							<span>9.8</span>
+							<p>Our score</p>
+						</div>
+					</div>										
 					<div class="interior-card-item-6-visit">
 						<span>
 							<form action="http://www.mail.ru" target="_blank">
-							    <button type="submit"><img src="img/button_visit_arrow.png" alt="Visit"><p>Visit Our Site</p></button>
+							    <button type="submit"><p>Visit Our Site</p></button>
 							</form>
 						</span>						
 					</div>
 				</div>
 
-				<div filter="p" class="cards card-item cards-mob" id="card-7">
+				<div filter="s" class="cards card-item cards-mob" id="card-7">
 					<div class="interior-card-item-1-number">
 						<span>7</span>
 					</div>
@@ -306,13 +306,7 @@
 							<h1>Delectus ea quibusdam incidunt, debitis!</h1>
 						</span>
 					</div>
-					<div class="interior-card-item-4-score">
-						<div class="our-score">
-							<span>9.8</span>
-							<p>Our score</p>
-						</div>
-					</div>					
-					<div class="interior-card-item-5-rating">
+					<div class="interior-card-item-4-rating">
 						<div class="ratings">
 							<div class="rate_message"></div>
 							<div class="rating not-rated" data-rate-value="3"></div>
@@ -320,17 +314,23 @@
 								<a href="review-single.php">Review</a>
 							</div>
 						</div>
-					</div>					
+					</div>
+					<div class="interior-card-item-5-score">
+						<div class="our-score">
+							<span>9.8</span>
+							<p>Our score</p>
+						</div>
+					</div>										
 					<div class="interior-card-item-6-visit">
 						<span>
 							<form action="http://www.mail.ru" target="_blank">
-							    <button type="submit"><img src="img/button_visit_arrow.png" alt="Visit"><p>Visit Our Site</p></button>
+							    <button type="submit"><p>Visit Our Site</p></button>
 							</form>
 						</span>						
 					</div>
 				</div>
 
-				<div filter="p" class="cards card-item cards-mob" id="card-8">
+				<div filter="s" class="cards card-item cards-mob" id="card-8">
 					<div class="interior-card-item-1-number">
 						<span>8</span>
 					</div>
@@ -344,13 +344,7 @@
 							<h1>Delectus ea quibusdam incidunt, debitis!</h1>
 						</span>
 					</div>
-					<div class="interior-card-item-4-score">
-						<div class="our-score">
-							<span>9.8</span>
-							<p>Our score</p>
-						</div>
-					</div>					
-					<div class="interior-card-item-5-rating">
+					<div class="interior-card-item-4-rating">
 						<div class="ratings">
 							<div class="rate_message"></div>
 							<div class="rating not-rated" data-rate-value="3"></div>
@@ -358,17 +352,23 @@
 								<a href="review-single.php">Review</a>
 							</div>
 						</div>
-					</div>					
+					</div>
+					<div class="interior-card-item-5-score">
+						<div class="our-score">
+							<span>9.8</span>
+							<p>Our score</p>
+						</div>
+					</div>										
 					<div class="interior-card-item-6-visit">
 						<span>
 							<form action="http://www.mail.ru" target="_blank">
-							    <button type="submit"><img src="img/button_visit_arrow.png" alt="Visit"><p>Visit Our Site</p></button>
+							    <button type="submit"><p>Visit Our Site</p></button>
 							</form>
 						</span>						
 					</div>
 				</div>
 
-				<div filter="p" class="cards card-item cards-mob" id="card-9">
+				<div filter="s" class="cards card-item cards-mob" id="card-9">
 					<div class="interior-card-item-1-number">
 						<span>9</span>
 					</div>
@@ -382,13 +382,7 @@
 							<h1>Delectus ea quibusdam incidunt, debitis!</h1>
 						</span>
 					</div>
-					<div class="interior-card-item-4-score">
-						<div class="our-score">
-							<span>9.8</span>
-							<p>Our score</p>
-						</div>
-					</div>					
-					<div class="interior-card-item-5-rating">
+					<div class="interior-card-item-4-rating">
 						<div class="ratings">
 							<div class="rate_message"></div>
 							<div class="rating not-rated" data-rate-value="3"></div>
@@ -396,17 +390,23 @@
 								<a href="review-single.php">Review</a>
 							</div>
 						</div>
-					</div>					
+					</div>
+					<div class="interior-card-item-5-score">
+						<div class="our-score">
+							<span>9.8</span>
+							<p>Our score</p>
+						</div>
+					</div>										
 					<div class="interior-card-item-6-visit">
 						<span>
 							<form action="http://www.mail.ru" target="_blank">
-							    <button type="submit"><img src="img/button_visit_arrow.png" alt="Visit"><p>Visit Our Site</p></button>
+							    <button type="submit"><p>Visit Our Site</p></button>
 							</form>
 						</span>						
 					</div>
 				</div>
 
-				<div filter="p" class="cards card-item cards-mob" id="card-10">
+				<div filter="s" class="cards card-item cards-mob" id="card-10">
 					<div class="interior-card-item-1-number">
 						<span>10</span>
 					</div>
@@ -420,13 +420,7 @@
 							<h1>Delectus ea quibusdam incidunt, debitis!</h1>
 						</span>
 					</div>
-					<div class="interior-card-item-4-score">
-						<div class="our-score">
-							<span>9.8</span>
-							<p>Our score</p>
-						</div>
-					</div>					
-					<div class="interior-card-item-5-rating">
+					<div class="interior-card-item-4-rating">
 						<div class="ratings">
 							<div class="rate_message"></div>
 							<div class="rating not-rated" data-rate-value="3"></div>
@@ -434,11 +428,17 @@
 								<a href="review-single.php">Review</a>
 							</div>
 						</div>
-					</div>					
+					</div>
+					<div class="interior-card-item-5-score">
+						<div class="our-score">
+							<span>9.8</span>
+							<p>Our score</p>
+						</div>
+					</div>										
 					<div class="interior-card-item-6-visit">
 						<span>
 							<form action="http://www.mail.ru" target="_blank">
-							    <button type="submit"><img src="img/button_visit_arrow.png" alt="Visit"><p>Visit Our Site</p></button>
+							    <button type="submit"><p>Visit Our Site</p></button>
 							</form>
 						</span>						
 					</div>
