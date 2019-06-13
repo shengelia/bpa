11ae4bf7d4f4bcacc3143a67b1fe7f9e9c993a12
Samira Shengelia <samira.shengelia@gmail.com>
2019-06-14 01:07:21 +0400

    changes, be careful in header

--- 00 - structure/components/header.php
+++ 00 - structure/components/header.php
@@ -8,34 +8,34 @@
 		            <span class="icon-bar"></span>
 	            	<span class="icon-bar"></span>
 	            </button>
-	            <a class="navbar-brand website-logo" href="index-1.php">
+	            <a class="navbar-brand website-logo" href="index.html">
 	            	<div class="date">
 	            		<img src="img/date.png" alt="Current Date" width="100%" height="100%">
 	            		<span id="current_date"></span>
 	            	</div>
 	            </a>
 	            <span class="language-selection-mobile-1 desktop-hidden">
-	            	<a href="index-1.php"><img src="img/ger.png" class="language-selection" alt="Language Selection" width="100%"></a>
+	            	<a href="index.html"><img src="img/ger.png" class="language-selection" alt="Language Selection" width="100%"></a>
 	            </span>
 	            <span class="language-selection-mobile-2 desktop-hidden">
-	            	<a href="index-1.php"><img src="img/ger.png" class="language-selection" alt="Language Selection" width="100%"></a>
+	            	<a href="index.html"><img src="img/ger.png" class="language-selection" alt="Language Selection" width="100%"></a>
 	            </span>
 	        </div>
 	        <div id="navbar" class="navbar-collapse collapse">
 				<ul class="nav navbar-nav navbar-right">
-					<li class="active"><a href="index-1.php">Home</a></li>
-					<li><a href="blackjack.php">Blackjack</a></li>
-		            <li><a href="roulette.php">Roulette</a></li>
-		            <li><a href="poker.php">Poker</a></li>
-		            <li><a href="baccarat.php">Baccarat</a></li>
-		            <li><a href="slots.php">Slots</a></li>
-		            <li><a href="review-all.php">Reviews</a></li>
-					<li><a href="score.php">How we score?</a></li>
+					<li class="active"><a href="index.html">Home</a></li>
+					<li><a href="blackjack.html">Blackjack</a></li>
+		            <li><a href="roulette.html">Roulette</a></li>
+		            <li><a href="poker.html">Poker</a></li>
+		            <li><a href="baccarat.html">Baccarat</a></li>
+		            <li><a href="slots.html">Slots</a></li>
+		            <li><a href="review-all.html">Reviews</a></li>
+					<li><a href="score.html">How we score?</a></li>
 					<li class="mobile-hidden-768">
-						<a href="index-1.php"><img src="img/ger.png" class="language-selection" alt="Language Selection" width="100%"></a>
+						<a href="index.html"><img src="img/ger.png" class="language-selection" alt="Language Selection" width="100%"></a>
 					</li>
 					<li class="mobile-hidden-768">
-						<a href="index-1.php"><img src="img/ger.png" class="language-selection" alt="Language Selection" width="100%"></a>
+						<a href="index.html"><img src="img/ger.png" class="language-selection" alt="Language Selection" width="100%"></a>
 					</li>
 				</ul>
 	        </div>
--- 00 - structure/score.php
+++ 00 - structure/score.php
@@ -12,12 +12,9 @@
 
 <?php include "components/slider.php";?>
 
-<section class="website-score">
+<!-- <section class="website-score">
 	<div class="container">
 		<div class="website-score-inner">
-			<div class="col-lg-3 col-md-6 col-sm-12 col-xs-12 review-item">
-				
-			</div>
 			<div class="col-lg-3 col-md-6 col-sm-12 col-xs-12 review-item">
 				<div class="review-logo">
 					<img src="img/score_02.png" alt="How We Score" width="100%" height="100%">
@@ -35,21 +32,18 @@
 			</div>
 		</div>
 	</div>
-</section>
+</section> -->
 
 <section class="website-how-we-score how-we-score-sc--s--">
     <div class="container">
         <div class="list--s-- flex--s-- wrap--s-- j-sp-between--s-- bc-how-we-score-container">
         	<div class="row">
 	        	<div class="bc-row col-lg-12 col-md-12 col-sm-12 col-xs-12">
-	        		<div class="col-25--s-- t-center--s-- wow fadeInUp col-lg-3 col-md-3 col-sm-3 col-xs-6 review-item" data-wow-delay="0.5s">
+	        		<div class="col-lg-3 col-md-6 col-sm-12 col-xs-12 col-25--s-- t-center--s-- review-item">
 	        			<div class="review-logo">
 	        				<img src="img/score_01.png" alt="How We Score" width="100%" height="100%">
 	        			</div>
-	        		    <div>
-	        		        <div class="img-outer--s--">
-	        		            <img src="img/hws-1.png" alt="icon1" />
-	        		        </div>
+	        		    <div class="review-inner-content">
 	        		        <p>Variety of Games</p>
 	        		        <img src="img/more.png" alt="Read more" width="100%" class="hws-read-more">
 	        		        <div class="info--s--">
@@ -61,15 +55,16 @@
 	        		            The enormous number of diverse games is offered to our special customers!
 	        		            The enormous number of diverse games is offered to our special customers!
 	        		            The enormous number of diverse games is offered to our special customers!
+	        		            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione, accusantium mollitia. Asperiores rerum perspiciatis, aut necessitatibus quasi in nostrum ratione et est sunt deleniti quisquam vitae saepe, nobis dolore pariatur voluptates cupiditate nulla laboriosam odio nam? Recusandae esse facere corrupti, porro quisquam adipisci, quod voluptatibus in, necessitatibus laboriosam deserunt id?
 	        		        </div>
 	        		    </div>
 	        		</div>
 
-	        		<div class="col-25--s-- t-center--s-- wow fadeInUp col-lg-3 col-md-3 col-sm-3 col-xs-6 review-item" data-wow-delay="0.5s">
-	        		    <div>
-	        		        <div class="img-outer--s--">
-	        		            <img src="img/hws-2.png" alt="icon2"/>
-	        		        </div>
+	        		<div class="col-lg-3 col-md-6 col-sm-12 col-xs-12 col-25--s-- t-center--s-- review-item">
+	        			<div class="review-logo">
+	        				<img src="img/score_02.png" alt="How We Score" width="100%" height="100%">
+	        			</div>
+	        		    <div class="review-inner-content">
 	        		        <p>Size and Number of Jackpots</p>
 	        		        <img src="img/more.png" alt="Read more" width="100%" class="hws-read-more">
 	        		        <div class="info--s--">
@@ -81,11 +76,11 @@
 	        		    </div>
 	        		</div>
 
-	        		<div class="col-25--s-- t-center--s-- wow fadeInUp col-lg-3 col-md-3 col-sm-3 col-xs-6 review-item" data-wow-delay="0.5s">
-	        		    <div>
-	        		        <div class="img-outer--s--">
-	        		            <img src="img/hws-3.png" alt="icon3"/>
-	        		        </div>
+	        		<div class="col-lg-3 col-md-6 col-sm-12 col-xs-12 col-25--s-- t-center--s-- review-item">
+	        			<div class="review-logo">
+	        				<img src="img/score_03.png" alt="How We Score" width="100%" height="100%">
+	        			</div>
+	        		    <div class="review-inner-content">
 	        		        <p>Loyalty Rewards</p>
 	        		        <img src="img/more.png" alt="Read more" width="100%" class="hws-read-more">
 	        		        <div class="info--s--">
@@ -97,11 +92,11 @@
 	        		    </div>
 	        		</div>
 
-	        		<div class="col-25--s-- t-center--s-- wow fadeInUp col-lg-3 col-md-3 col-sm-3 col-xs-6 review-item" data-wow-delay="0.5s">
-	        		    <div>
-	        		        <div class="img-outer--s--">
-	        		            <img src="img/hws-4.png" alt="icon4"/>
-	        		        </div>
+	        		<div class="col-lg-3 col-md-6 col-sm-12 col-xs-12 col-25--s-- t-center--s-- review-item">
+	        			<div class="review-logo">
+	        				<img src="img/score_04.png" alt="How We Score" width="100%" height="100%">
+	        			</div>
+	        		    <div class="review-inner-content">
 	        		        <p>Other Entertainment</p>
 	        		        <img src="img/more.png" alt="Read more" width="100%" class="hws-read-more">
 	        		        <div class="info--s--">
