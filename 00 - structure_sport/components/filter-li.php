5dd2f4d887f178d58b720859ca9c0700a6daed1a
samira <s.shengelia@bpa-solutions.org>
2019-07-17 15:53:01 +0400

    changes

--- 00 - structure_sport/components/filter-li.php
+++ 00 - structure_sport/components/filter-li.php
@@ -1,6 +1,6 @@
 <li filter="all" class="filter_active"><a href="#">View all</a></li>
-<li filter="cric"><img src="img/cric.png" alt=""><a href="#">Cricket</a></li>
-<li filter="afl"><img src="img/afl.png" alt=""><a href="#">AFL</a></li>
-<li filter="socc"><img src="img/socc.png" alt=""><a href="#">Soccer</a></li>
-<li filter="hors"><img src="img/hors.png" alt=""><a href="#">Horse Racing</a></li>
-<li filter="golf"><img src="img/golf.png" alt=""><a href="#">Golf</a></li>
+<li filter="cric"><a href="#">Cricket</a></li>
+<li filter="afl"><a href="#">AFL</a></li>
+<li filter="socc"><a href="#">Soccer</a></li>
+<li filter="hors"><a href="#">Horse Racing</a></li>
+<li filter="golf"><a href="#">Golf</a></li>
